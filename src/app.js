512685b29947fb03a459decd40a1cfdc3d309a76
azrilmiqrajiii <azrilmiqraji.210602005@student.hamzanwadi.ac.id>
2026-01-14 06:13:21 +0800

    lagi

--- src/app.js
+++ src/app.js
@@ -2,31 +2,44 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const path = require("path");
+
 const app = express();
+
+const allowedOrigins = [
+  "https://frontend-virid-ten-88.vercel.app",
+  "http://localhost:5173",
+];
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: function (origin, callback) {
+      // allow non-browser requests (Postman, curl)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(cookieParser());
-app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
-app.use("/uploads", express.static("uploads"));
-app.use("/api/visi-misi", require("./routes/visiMisi.routes"));
-app.use("/uploads", express.static("uploads"));
 
+// static
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
+
+// routes
 app.get("/", (req, res) => {
   res.send("SPM Backend OK");
 });
 
-const authRoutes = require("./routes/auth.routes");
-app.use("/api/auth", authRoutes);
-
-const menuRoutes = require("./routes/menu.routes");
-app.use("/api/menus", menuRoutes);
+app.use("/api/visi-misi", require("./routes/visiMisi.routes"));
+app.use("/api/auth", require("./routes/auth.routes"));
+app.use("/api/menus", require("./routes/menu.routes"));
 
 module.exports = app;
