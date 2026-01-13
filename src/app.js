9d52fa89ed8a019a47dcae5bb289131bcdfd72b6
azrilmiqrajiii <azrilmiqraji.210602005@student.hamzanwadi.ac.id>
2026-01-14 05:59:47 +0800

    perbaiki app.js lagi di cors

--- src/app.js
+++ src/app.js
@@ -4,15 +4,14 @@ const cookieParser = require("cookie-parser");
 const path = require("path");
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 app.use(
   cors({
     origin: process.env.CLIENT_URL,
     credentials: true,
   })
 );
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
